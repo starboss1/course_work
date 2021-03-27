7b0d0ffd251e2e806679e1dbce3149b61de12eeb
Sergei <sergei.konoshenko2000@gmail.com>
2021-03-27 15:26:37 +0200

    Fix profile page

--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -65,31 +65,51 @@ const Profile = () => {
 
     return (
         <div className="container">
-            <header className="jumbotron">
+            {/* <header className="jumbotron">
                 <h3>
                     <strong>{currentUser.username}</strong> Профіль
                 </h3>
-            </header>
-            <p>
-                <strong>Токен:</strong>{" "}
-                {currentUser.accessToken.substring(0, 20)} ...{" "}
-                {currentUser.accessToken.substr(
-                    currentUser.accessToken.length - 20
-                )}
-            </p>
-            <p>
-                <strong>Id:</strong> {currentUser.id}
-            </p>
-            <p>
-                <strong>Пошта:</strong> {currentUser.email}
-            </p>
-            <strong>Ролі:</strong>
-            <ul>
-                {currentUser.roles &&
-                    currentUser.roles.map((role, index) => (
-                        <li key={index}>{role}</li>
-                    ))}
-            </ul>
+            </header> */}
+            <div className="text-center">
+                <svg
+                    width="200"
+                    height="200"
+                    viewBox="0 0 200 200"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="100" cy="100" r="99.5" stroke="black" />
+                    <path d="M95.84 57V141H102.44V57H95.84Z" fill="black" />
+                </svg>
+            </div>
+            <div className="text-center display-4 my-2">
+                <span>{currentUser.username}</span>
+            </div>
+
+            <div className="border">
+                <div className="py-4 px-4">
+                    <div className="form-group">
+                        <span className="mr-2 font-weight-bold">
+                            Ім'я: 
+                        </span>
+                        <span> {currentUser.username}</span>
+                    </div>
+                    <div className="form-group">
+                        <span className="mr-2 font-weight-bold">
+                            Пошта 
+                        </span>
+                        <span> {currentUser.email}</span>
+                    </div>
+                    <div className="form-group">
+                        <span className="mr-2 font-weight-bold">
+                            Роль: 
+                        </span>
+                        <span> {currentUser.roles.map((role, index) => (
+                            <span className="mr-1">{role === 'ROLE_USER'? 'Користувач': ''}</span>
+                        ))}</span>
+                    </div>   
+                </div>
+            </div>
             <h3>Мої документи</h3>
             <div className="row d-flex justify-content-start">{ownerDocumentList}</div>
             <h3>Доступні документи</h3>
